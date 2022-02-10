26603f08964b743ce06c4c757752248222bf2da0
ppofiant <ppofiant26@gmail.com>
2022-02-10 20:18:02 +0700

    release handle responsive

--- pages/contactme.tsx
+++ pages/contactme.tsx
@@ -34,12 +34,12 @@ const ContactMePage: React.FunctionComponent<{
                     <h1 className="title-contact">GET IN TOUCH</h1>
                     <p className="text-center title-description">Feel free to ask anything about me through these contact. or follow my instagram instead to see my photo collections !</p>
                 </div>
-                <div className="row mx-0" data-aos="zoom-out-up">
+                <div className="row mx-0 contact-wrapper" data-aos="zoom-out-up">
                     <div className="col-lg-6">
                         <ButtonContact id={'linkedin'} logoSrc="/images/linkedin.png" href="https://www.linkedin.com/in/popo-fianto-25b678198/">Popo Fianto</ButtonContact>
                     </div>
                     <div className="col-lg-6">
-                        <ButtonContact id={'instagram'} logoSrc="/images/instagram.png" href="https://github.com/ppofiant">ppofiant</ButtonContact>
+                        <ButtonContact id={'instagram'} logoSrc="/images/instagram.png" href="https://instagram.com/ppofiant">ppofiant</ButtonContact>
                     </div>
                     <div className="col-lg-6">
                         <ButtonContact id={'email'} logoSrc="/images/gmail.png" href="mailto:ppofiant26@gmail.com">ppofiant26@gmail.com</ButtonContact>
